f4f78f44a35630aa611681c22a9823c10e6570e5
frademabr <yasmin@fradema.com.br>
2025-01-13 19:48:28 -0300

    first commit

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  semi: true,
+  tabWidth: 2,
+  endOfLine: "lf",
+  printWidth: 90,
+  singleQuote: false,
+  trailingComma: "all",
+};
+
+export default config;
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+
+//--------------------------------------------------------------------------------------
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#000000",
+};
+
+export const metadata: Metadata = {
+  title: "Fradema Consultoria Tributária",
+  description: "Empresa Fradema de Consultoria Tributária",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
